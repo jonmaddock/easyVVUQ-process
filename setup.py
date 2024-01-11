bdb05a3d27fe21c510626a900f17fb8151fc43a1
Jonathan Maddock <78556175+jonmaddock@users.noreply.github.com>
2024-01-11 08:55:38 +0000

    Add installation instructions

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name="infeas", version="0.1", packages=find_packages())
+setup(
+    name="infeas",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "easyvvuq",
+        "dask",
+        "matplotlib",
+        "seaborn",
+        "numpy",
+        "pandas",
+    ],
+)
